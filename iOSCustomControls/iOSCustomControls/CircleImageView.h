c51ac80c69fb982301f78df930e1d351e7b29f55
Donovan Palma <dpalma@infusion.com>
2013-10-28 18:08:53 -0400

    adding logic to draw nested arcs

---
+++ iOSCustomControls/iOSCustomControls/CircleImageView.h
@@ -0,0 +1,13 @@
+//
+//  CircleImageView.h
+//  iOSCustomControls
+//
+//  Created by Donovan Palma on 2013-10-28.
+//  Copyright (c) 2013 Donovan Palma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleImageView : UIView
+
+@end
---
+++ iOSCustomControls/iOSCustomControls/CircleImageView.m
@@ -0,0 +1,69 @@
+//
+//  CircleImageView.m
+//  iOSCustomControls
+//
+//  Created by Donovan Palma on 2013-10-28.
+//  Copyright (c) 2013 Donovan Palma. All rights reserved.
+//
+
+#import "CircleImageView.h"
+
+@implementation CircleImageView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    [super drawRect:rect];
+    
+    // Drawing code
+    CGRect myRect = CGRectMake(0, 0, self.bounds.size.width, self.bounds.size.height);
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    //    UIGraphicsBeginImageContext(myRect.size);
+    
+    
+    UIImage *image = [UIImage imageNamed:@"1338463987.jpg"];
+    
+    float newWidth = rect.size.height;
+    float newHeight = newWidth/(image.size.width/image.size.height);
+    
+    CGRect newRect = CGRectMake(0, 0, newWidth, newHeight);
+    
+    CGContextSetAllowsAntialiasing(context, true);
+    CGContextSetShouldAntialias(context, true);
+    CGContextSetInterpolationQuality(context, kCGInterpolationHigh);
+    
+    CGContextSetFillColorWithColor(context, [UIColor orangeColor].CGColor);
+    
+    CGContextAddArc(context, newRect.size.width/2, newRect.size.height/2, (newRect.size.height/2), 0, 2*M_PI, 1);
+
+    CGContextEOClip(context);
+    CGContextSetFillColorWithColor(context, [UIColor purpleColor].CGColor);
+    CGContextFillRect(context, self.bounds);
+
+    CGContextAddArc(context, newRect.size.width/2, newRect.size.height/2, (newRect.size.height/2)-6, 0, 2*M_PI, 1);
+
+    CGContextEOClip(context);
+
+    CGContextSaveGState(context);
+
+    CGContextRotateCTM(context,180*M_PI/180);
+    CGContextTranslateCTM(context, newRect.size.width*-1, newRect.size.height*-1);
+    CGContextDrawImage(context, newRect, image.CGImage);
+    
+    CGContextRestoreGState(context);
+}
+
+
+@end
