6a553d2c7ff67d52609eeceac3ec8b3a9c398d1f
Donovan Palma <dpalmajr@Donovans-MacBook-Pro.local>
2013-10-30 22:15:40 -0400

    removing commented code

--- iOSCustomControls/iOSCustomControls/CircularImageView.m
+++ iOSCustomControls/iOSCustomControls/CircularImageView.m
@@ -20,15 +20,6 @@
 }
 
 
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-//- (void)drawRect:(CGRect)rect
-//{
-//    // Drawing code
-////    [super drawRect:rect];
-//    
-//}
-
 -(void)willMoveToSuperview:(UIView *)newSuperview{
     //generate mask that will be applied to imageView
     CALayer *maskLayer = [[CALayer alloc]init];
